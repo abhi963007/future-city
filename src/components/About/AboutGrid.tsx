34a9b2b17e0031fa621d7733bd70c4b234d63ee3
abhi963007 <ahiramak963@gmail.com>
2026-08-03 13:15:51 +0530

    feat: add AboutGrid component with image gallery and center video panel

--- src/components/About/AboutGrid.tsx
+++ src/components/About/AboutGrid.tsx
@@ -149,9 +149,7 @@ const AboutGrid: React.FC = () => {
                   playsInline
                   style={{ objectFit: 'cover', width: '100%', height: '100%' }}
                 >
-                  <source src="/images/center video.mp4" type="video/mp4" />
-                  <source src="/media/grid-center-video.webm" type="video/webm" />
-                  <source src="/media/grid-center-video.mp4" type="video/mp4" />
+                  <source src="/media/about-center.mp4" type="video/mp4" />
                 </video>
               </div>
 
